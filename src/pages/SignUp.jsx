a137cb09da9ac42adb269d5bec07316633c2487a
SIMPL3IFY <thinhnguyen.student@gmail.com>
2026-09-13 20:13:04 -0700

    Fixing merge conflict for Tuong branch, which cause by my merge into main

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -33,10 +33,11 @@ export default function SignUp() {
     } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
       newErrors.email = 'Please enter a valid email address'
     }
+    
     if (!formData.password) {
       newErrors.password = 'Password is required'
     } else if (formData.password.length < 8) {
-      newErrors.password = 'Password must be at least 8 characters' 
+      newErrors.password = 'Password must be at least 8 characters'
     }
     
     if (!formData.confirmPassword) {
