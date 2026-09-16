585e08441e5b6c842afc8303993b72fcf5e7377d
Your Name <thinhnguyen.student@gmail.com>
2026-09-16 12:24:49 -0700

    minor fix to import feature; Supabase database. And update to latest version.

--- src/pages/CustomerPortal.jsx
+++ src/pages/CustomerPortal.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 //import { Link } from 'react-router-dom'
 import { SERVICES_LIST } from '../data/ServicesData';
 import { Link, useNavigate } from 'react-router-dom'
-import { supabase } from '../supabaseClient'
+import { supabase } from '../lib/supabaseClient'
 
 export default function CustomerPortal() {
     const navigate = useNavigate()
